97f41f734e27d85c3ec6543ada3da9f8350c5a1b
patmilane <patriciahmilane@hotmail.com>
2022-01-28 15:23:15 -0300

    atualizando icone e identação

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
-function consultar() {
-    let data = $(".data").val();
-    const chave = 'dNNwrKhvOInpkMrKEmgsoja5JwVjMgnlOdiYIyDB';
-
-    $.ajax({
-        url: `https://api.nasa.gov/planetary/apod?api_key=${chave}&date=${data}`,
-        success: function (resultado) {
-            mostrarResultado(resultado);
-        },
-        error: function () {
-            $(".mensagemErro").html("Data inválida").css({color: "red"})
-        }
-    })
-}
-
-
-function mostrarResultado(consulta) {
-    const data = $("#data")
-    const midia = $("#midia")
-    const titulo = $("#titulo")
-    const autor = $("#autor")
-    const legenda = $("#legenda")
-
-
-    data.html(moment(consulta.date).format('MMMM Do YYYY'))
-    titulo.html(consulta.title);
-    autor.html(`Créditos: ${consulta.copyright}`);
-    legenda.html(`Sobre: ${consulta.explanation}`);
-
-
-    if (consulta.media_type == 'image') {
-        midia.html(`<img id="midiaJS" src="${consulta.url}" />`);
-    } else {
-        midia.html(`<iframe id="midiaJS" src="${consulta.url}?autoplay=1&mute=1"></iframe>`);
-    }
-}
-
-function inicio(){
+function inicio() {
     location = "index.html";
 }
 
-function pesquisar(){
-    location = "pesquisar.html"
+function pesquisar() {
+    location = "pesquisar.html";
+}
+
+function pesquisarData() {
+    let data = $(".data").val();
+    const apiKey = 'Sk0oacheSAeMtk6f8altpm2U0Y6OHOmxlOU4ncTU';
+
+    $.ajax({ url: `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${data}`,
+        success: function (resultado) {
+            resultadoPesquisa(resultado);
+        },
+        error: function () {
+            $(".mensagemErro").html("Data inválida").css({color: "red"});
+        }
+    });
+}
+
+function resultadoPesquisa(pesquisa) {
+    const data = $("#data");
+    const midia = $("#midia");
+    const titulo = $("#titulo");
+    const autor = $("#autor");
+    const legenda = $("#legenda");
+
+    data.html(moment(pesquisa.date).format('DD MMMM, YYYY'));
+    titulo.html(pesquisa.title);
+    autor.html(`Créditos: ${pesquisa.copyright}`);
+    legenda.html(`Sobre: ${pesquisa.explanation}`);
+
+    if (pesquisa.media_type == 'image') {
+        midia.html(`<img id="midiaJS" src="${pesquisa.url}"/>`);
+    } else {
+        midia.html(`<iframe id="midiaJS" src="${pesquisa.url}?autoplay=1&mute=1"></iframe>`);
+    }
 }
